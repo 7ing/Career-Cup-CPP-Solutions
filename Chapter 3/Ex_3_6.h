23c5db1dbbade6a7520b3bdd61300990cc12d65d
jliu <mitkook@gmail.com>
2012-08-19 14:35:44 -0500

    Done with Ex 3.6

--- Chapter 3/Ex_3_6.h
+++ Chapter 3/Ex_3_6.h
@@ -19,17 +19,43 @@
 
 #include <iostream>
 #include <stack>
+#include <cstdlib>
 using namespace std;
 
+/*
+ * using insert sort idea
+ * O(n^2) time, O(n) space
+ */
 template<class T>
-void sort_stack_func (stack<T> *st) {
-
+stack<T> sort_stack_func(stack<T> &st) {
+	stack<T> temp;
+	while (!st.empty()) {
+		T insert_val = st.top();
+		st.pop();
+		while (!temp.empty() && temp.top() > insert_val) {
+			st.push(temp.top());
+			temp.pop();
+		}
+		temp.push(insert_val);
+	}
+	return temp;
 }
 
 void test() {
+	stack<int> st;
+	cout << "stack from bottom to top: ";
+	for (int i = 0; i < 10; i++) {
+		int num = rand() % 100;
+		st.push(num);
+		cout << num << ", ";
+	}
+	cout << endl << "after sort, from top to bottom: ";
+	st = sort_stack_func(st);
+	while (!st.empty()) {
+		cout << st.top() << ", ";
+		st.pop();
+	}
 
 }
-
-
 
 #endif /* EX_3_6_H_ */
