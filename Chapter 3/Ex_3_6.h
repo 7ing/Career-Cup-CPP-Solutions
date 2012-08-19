d3692bec87707d77287d193cbcae45b7a70851e6
jliu <mitkook@gmail.com>
2012-08-19 00:04:25 -0500

    Initialize Ex 3.6

---
+++ Chapter 3/Ex_3_6.h
@@ -0,0 +1,35 @@
+/*
+ * Ex_3_6.h
+ *
+ *  Created on: Aug 18, 2012
+ *      Author: jliu
+ */
+
+/*
+ * Write a program to sort a stack in ascending order. You should not make any assumptions
+ * about how the stack is implemented. The following are the only functions that should be
+ * used to write this program:
+ * push | pop | peek | isEmpty
+ *
+ * in C++ STL: push | pop | top | empty
+ */
+
+#ifndef EX_3_6_H_
+#define EX_3_6_H_
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+template<class T>
+void sort_stack_func (stack<T> *st) {
+
+}
+
+void test() {
+
+}
+
+
+
+#endif /* EX_3_6_H_ */
--- Test.cpp
+++ Test.cpp
@@ -9,7 +9,7 @@
  * include "Chapter X/Ex_X_Y.h" to test Exercise X.Y in Career Cup 4.0 Version
  */
 
-#include "Chapter 3/Ex_3_5.h"
+#include "Chapter 3/Ex_3_6.h"
 #include <iostream>
 
 using namespace std;
