d94361a8f95579717593d594bfe9bfde5a49781a
jliu <mitkook@gmail.com>
2012-08-18 21:14:09 -0500

    Linux Version Commit

---
+++ Chapter 3/Ex_3_5.h
@@ -0,0 +1,19 @@
+/*
+ * Ex_3_5.h
+ *
+ *  Created on: Aug 18, 2012
+ *      Author: jliu
+ */
+
+#ifndef EX_3_5_H_
+#define EX_3_5_H_
+
+#include <iostream>
+using namespace std;
+
+void test() {
+	cout<<"queue using two stacks."<<endl;
+}
+
+
+#endif /* EX_3_5_H_ */
