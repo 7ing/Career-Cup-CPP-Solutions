002a04f436f5fc9b35b9ad037df18315e41a4871
jliu <mitkook@gmail.com>
2012-08-30 00:17:47 -0500

    init ex 5.1

---
+++ Chapter 5/Ex_5_1.h
@@ -0,0 +1,23 @@
+/*
+ * Ex_5_1.h
+ *
+ *  Created on: Aug 30, 2012
+ *      Author: jliu
+ */
+
+/*
+ * You are given two 32-bit numbers, N and M, and two bit positions, i and j.
+ * Write a method to set all bits between i and j in N equal to M (e.g., M becomes
+ * a substring of N located at i and starting at j).
+ * EXAMPLE:
+ * Input: N = 10000000000, M = 10101, i=2,j=6
+ * Output:N = 10001010100
+ */
+
+#ifndef EX_5_1_H_
+#define EX_5_1_H_
+
+
+
+
+#endif /* EX_5_1_H_ */
