1eb32c8b17b1d280d5b15c03a8ecc5c615262703
jliu <mitkook@gmail.com>
2012-08-30 23:07:34 -0500

    Done with Ex 5.1

--- Chapter 5/Ex_5_1.h
+++ Chapter 5/Ex_5_1.h
@@ -17,7 +17,38 @@
 #ifndef EX_5_1_H_
 #define EX_5_1_H_
 
+#include<iostream>
+#include<bitset>
+using namespace std;
+
+void setBits(int & N, int M, int i, int j) {
+	// produce mask number
+	int mask = 0;
+	for(;j>=i;j--)
+		mask = (mask<<1) | 1;
+	M = M & mask; // remove sign bit
+	mask = ~(mask<<i);
+	M = M<<i;
+	N = (N & mask) | M;
+}
+
+void test() {
+
+	int N = 1 << 10; // N = 10000000000
+	bitset<32> N_bit(N);
+	cout<<"N: "<<N_bit.to_string()<<endl;
+
+	int M = 1 | 4 | 16; // M = 10101
+	bitset<32> M_bit(M);
+	cout<<"M: "<<M_bit.to_string()<<endl;
+
+	int i = 2, j = 6;
+	setBits(N, M, i, j);
+	cout<<"set bits of N from i: "<<i<<" to j: "<<j<<" : "<<endl;
+	bitset<32> result(N);
+	cout<<"N: "<<result.to_string()<<endl;
 
 
+}
 
 #endif /* EX_5_1_H_ */
--- Test.cpp
+++ Test.cpp
@@ -9,7 +9,7 @@
  * include "Chapter X/Ex_X_Y.h" to test Exercise X.Y in Career Cup 4.0 Version
  */
 
-#include "Chapter 4/Ex_4_8.h"
+#include "Chapter 5/Ex_5_1.h"
 #include <iostream>
 
 using namespace std;
